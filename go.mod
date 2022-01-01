dc2bbd7b1eb9ce2e5a3dfa12f83f33faf84bc74f
retrorocket <mail@retrorocket.biz>
2022-01-01 23:43:48 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,32 @@
+module oauth2-client
+
+go 1.17
+
+require github.com/labstack/echo/v4 v4.6.1
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/garyburd/redigo v1.6.3 // indirect
+	github.com/go-redis/redis/v8 v8.11.4 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/labstack/echo-contrib v0.11.0 // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/boj/redistore.v1 v1.0.0-20160128113310-fc113767cd6b // indirect
+)
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/oauth2"
+	redisStore "gopkg.in/boj/redistore.v1"
+)
+
+var (
+	config = oauth2.Config{
+		ClientID:     os.Getenv("RC_CLIENT_ID"),
+		ClientSecret: os.Getenv("RC_CLIENT_SECRET"),
+		RedirectURL:  "http://localhost:18099/oauth2",
+		Scopes:       []string{"https://www.googleapis.com/auth/calendar"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://accounts.google.com/o/oauth2/auth",
+			TokenURL: "https://accounts.google.com/o/oauth2/token",
+		},
+	}
+)
+
+func RandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
+func main() {
+	// Echo instance
+	e := echo.New()
+	store, err := redisStore.NewRediStore(10, "tcp", ":6379", "", []byte(securecookie.GenerateRandomKey(32)))
+	if err != nil {
+		panic(err)
+	}
+	e.Use(session.Middleware(store))
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/try", func(c echo.Context) error {
+
+		sess, _ := session.Get("session", c)
+		sess.Options = &sessions.Options{
+			Path:     "/",
+			MaxAge:   60 * 5,
+			HttpOnly: true,
+		}
+		state := RandomString(20)
+		sess.Values["state"] = state
+		if err = sess.Save(c.Request(), c.Response()); err != nil {
+			return err
+		}
+
+		return c.Redirect(http.StatusSeeOther, config.AuthCodeURL(state))
+	})
+
+	// Routes
+	e.GET("/oauth2", func(c echo.Context) error {
+
+		stateParam := c.QueryParam("state")
+		sess, _ := session.Get("session", c)
+		state, _ := sess.Values["state"]
+		if state != stateParam {
+			return c.NoContent(http.StatusForbidden)
+		}
+		code := c.QueryParam("code")
+		if code == "" {
+			return c.NoContent(http.StatusForbidden)
+		}
+		token, err := config.Exchange(oauth2.NoContext, code)
+		if err != nil {
+			return err
+		}
+		sess.Options.MaxAge = -1
+		if err = sess.Save(c.Request(), c.Response()); err != nil {
+			return err
+		}
+		return c.Redirect(http.StatusSeeOther, "https://mlcpcfkgkgnpfmjplcjcegkolppjeaoa.chromiumapp.org#access_token="+token.AccessToken)
+
+	})
+
+	// Start server
+	e.Logger.Fatal(e.Start(":18099"))
+
+}
